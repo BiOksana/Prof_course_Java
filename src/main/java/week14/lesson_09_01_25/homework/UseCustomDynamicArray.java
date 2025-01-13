676be62c5cc95d0ef089eb6375b774ecc92d3c1d
BiOksana <biokissme@gmail.com>
2025-01-13 17:27:00 +0100

    Homework. Work with Multithreading

--- src/main/java/week14/lesson_09_01_25/homework/UseCustomDynamicArray.java
+++ src/main/java/week14/lesson_09_01_25/homework/UseCustomDynamicArray.java
@@ -1,12 +1,25 @@
 package week14.lesson_09_01_25.homework;
 
+import java.util.Iterator;
+
 public class UseCustomDynamicArray {
     public static void main(String[] args) {
         CustomDynamicArray customDynamicArray = new CustomDynamicArray();
 
         Thread thread1 = new Thread(() -> {
+            if (customDynamicArray.isEmpty()) {
             for (int i = 0; i < 100; i++) {
                 customDynamicArray.add(i + 1);
+            }
+            }
+        });
+
+        Thread thread11 = new Thread(() -> {
+            try {
+                thread1.join();
+                customDynamicArray.addAt(49, 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         });
 
@@ -14,6 +27,15 @@ public class UseCustomDynamicArray {
             try {
                 thread1.join();
                 customDynamicArray.remove();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread thread21 = new Thread(() -> {
+            try {
+                thread1.join();
+                customDynamicArray.removeAt(34);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -31,7 +53,7 @@ public class UseCustomDynamicArray {
         Thread thread4 = new Thread(() -> {
             try {
                 thread1.join();
-                for (int i = 0; i < 99; i++) {
+                for (int i = 0; i < 50; i++) {
                     System.out.println(customDynamicArray.get(i));
                 }
 
@@ -40,16 +62,35 @@ public class UseCustomDynamicArray {
             }
         });
 
+        Thread thread5 = new Thread(() -> {
+            try {
+                thread1.join();
+                Iterator<Integer> iterator = customDynamicArray.iterator();
+                while (iterator.hasNext()) {
+                    System.out.println(Thread.currentThread().getName() + " " + iterator.next());
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
         thread1.start();
+        thread11.start();
         thread2.start();
+        thread21.start();
         thread3.start();
         thread4.start();
+        thread5.start();
 
         try {
             thread1.join();
+            thread11.join();
             thread2.join();
+            thread21.join();
             thread3.join();
             thread4.join();
+            thread5.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
